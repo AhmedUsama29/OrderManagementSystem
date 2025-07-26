0718a4cdb7e5f1d38871387d39a39d531df2bfdf
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-07-26 21:15:28 +0300

    Created Identity db context & Migrations

--- Persistence/InfrastructureServicesRegisteration.cs
+++ Persistence/InfrastructureServicesRegisteration.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Data;
+using Persistence.Identity;
 
 
 namespace Persistence
@@ -15,10 +17,26 @@ namespace Persistence
             services.AddDbContext<OrderManagementDbContext>(options =>
                             options.UseSqlServer(configurations.GetConnectionString("OrderManagementConnection")));
 
+            services.AddDbContext<OrderManagementIdentityDbContext>(options =>
+                           options.UseSqlServer(configurations.GetConnectionString("OrderManagementIdentityConnection")));
+
+            services.RegisterIdentity();
 
             return services;
         }
 
+        private static IServiceCollection RegisterIdentity(this IServiceCollection services)
+        {
+
+            services.AddIdentityCore<IdentityUser>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+            })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<OrderManagementIdentityDbContext>();
+
+            return services;
+        }
 
     }
 }
