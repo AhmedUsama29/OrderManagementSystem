d5d0b7245b9113bfb956bccab47853f62f362820
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-07-27 00:21:22 +0300

    Add authentication service and user-related exceptions
    - Introduced `UserNotFoundException` for handling user not found scenarios.
    - Created `IAuthenticationService` interface with `LoginAsync` and `RegisterAsync` methods.
    - Implemented `AuthenticationService` to manage user authentication and error handling.
    - Added `LoginRequest` and `RegisterRequest` classes for encapsulating login and registration data.
    - Created `UserResponse` class to return user information after authentication.

---
+++ Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class UserNotFoundException(string username) : NotFoundException($"No User With This username : {username} Was Found")
+    {
+    }
+}
---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,92 @@
+﻿using Domain.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ServicesAbstraction;
+using Shared.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Services
+{
+    public class AuthenticationService(UserManager<IdentityUser> _userManager,
+                                        IOptions<JWTOptions> _jwtOptions) : IAuthenticationService
+    {
+     
+        public async Task<UserResponse> LoginAsync(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.Username)
+                ?? throw new UserNotFoundException(request.Username);
+
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, request.Password);
+
+            if (isPasswordValid)
+                return new UserResponse
+                {
+                    Username = user.UserName!,
+                    Token = await GenerateToken(user)
+                };
+
+            throw new UnAuthorizedException();
+        }
+
+        public async Task<UserResponse> RegisterAsync(RegisterRequest request)
+        {
+            var user = new IdentityUser()
+            {
+                UserName = request.Username,
+
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+
+            if (result.Succeeded) return new UserResponse()
+            {
+                Username = user.UserName,
+                Token = await GenerateToken(user)
+            };
+
+            var Errors = result.Errors.Select(e => e.Description).ToList();
+
+            throw new BadRequestException(Errors);
+        }
+
+        private async Task<string> GenerateToken(IdentityUser user)
+        {
+            var jwtOptions = _jwtOptions.Value;
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier, user.Id!),
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            string secretKey = jwtOptions.SecretKey;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtOptions.Issuer,
+                audience: jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(jwtOptions.DurationInDays),
+                signingCredentials: credentials
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
