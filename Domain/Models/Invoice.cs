9187971531dc07aca4a3e01f4231ec5000dcfca3
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-07-26 18:54:58 +0300

    Add project files.

---
+++ Domain/Models/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public Order Order { get; set; } // 1-1
+    }
+}
---
+++ Domain/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+
+    public enum PaymentMethod
+    {
+        CreditCard,
+        PayPal,
+        CashOnDelivery
+    }
+    public class Order
+    {
+
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; } // M-1
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public IEnumerable<OrderItem> OrderItems { get; set; } // 1-M
+        public PaymentMethod PaymentMethod { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
